a225e84a5ddb6ce89a8dd2616db7a2b5334c5587
Zhang Fei <zhangfei.eason@gmail.com>
2020-04-21 22:05:58 +0800

    docs: update #3 problem answer doc

--- src/main/kotlin/men/zhangfei/leetcode/medium/Problem0003.kt
+++ src/main/kotlin/men/zhangfei/leetcode/medium/Problem0003.kt
@@ -25,7 +25,7 @@ class Problem0003 {
                 var l = 0
                 var r = 0
                 val set: MutableSet<Char> = mutableSetOf()
-                while (l < len && r < len) {
+                while (r < len) {
                     if (set.contains(s[r])) {
                         set.remove(s[l++])
                         if (len - l <= max) {
@@ -40,21 +40,37 @@ class Problem0003 {
             }
         }
 
-        fun optimizedSlidingWindow(s: String): Int {
-            var max = 0
-            if (s.isNotEmpty()) {
-                val arr = s.toCharArray()
+        /**
+         * 优化的滑动窗口
+         * 0. 初始化窗口左边和右边的索引为 0 l = 0, r = 0
+         * 1. 窗口右边索引递增循环 r++; r < s.length
+         * 2. 如果当前右边索引下的字符已经存在，则判断 当前左边索引 > (该字符上次索引 + 1) l > (map[s[\r]] + 1)
+         *   a. 是：则说明 (该字符上次索引 + 1) 至 当前左边索引中有其它重复字符，所以忽略，不改变左边索引 l
+         *   b. 否：将左边索引移动到 (该字符上次索引 + 1) 的位置 l = (map[s[\r]] + 1)
+         * 4. 更新最长子串长度 max = max(已存储的最大长度, 当前窗口长度)
+         * 5. 将当前右边索引和字符存入 HashMap<字符，索引>. e.g. {"a": 0, "b": 1}
+         * 6. 转到 1 步
+         */
+        fun optimizedSlidingWindow(s: String): Int = when {
+            s.isEmpty() -> 0
+            s.length == 1 -> 1
+            else -> {
+                var max = 0
+                val len = s.length
                 var l = 0
+                var c: Char
                 val map: HashMap<Char, Int> = hashMapOf()
-                for ((i, c) in arr.withIndex()) {
+                for (r in 0 until len) {
+                    c = s[r]
                     map[c]?.let {
-                        l = it.coerceAtLeast(l)
+                        // 防止左边索引回跳
+                        l = l.coerceAtLeast(it + 1)
                     }
-                    max = max.coerceAtLeast(i - l + 1)
-                    map[c] = i + 1
+                    max = max.coerceAtLeast(r - l + 1)
+                    map[c] = r
                 }
+                max
             }
-            return max
         }
     }
 }
